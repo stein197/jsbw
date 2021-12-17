33c673e21e8158c77997bc5583fb38d9c1ac78f2
Nail Gafarov <nil20122013@gmail.com>
2021-12-17 14:50:24 +0500

    Completed tests

--- test.ts
+++ test.ts
@@ -3,7 +3,6 @@ import mocha from "mocha";
 import * as bw from ".";
 
 const b1101 = 0b1101;
-const b0 = 0;
 /*
 All functions should be tested against next numbers:
 - 0
@@ -15,47 +14,47 @@ All functions that accept indices should be tested against next indices:
 */
 
 mocha.describe("get()", () => {
-	"get(0, 0) === 0" // Getting bit at 0 from zero returns 0
-	"get(0, 1) === 0" // Getting bit at 1 from zero returns 0
-	"get(0b1101, 0) === 1" // Getting 1 bit at 0 returns 1
-	"get(0b1101, 1) === 0" // Getting 0 bit at 1 returns 0
-	"get(0b1101, 10) === 0" // Getting bit at large index returns 0
+	mocha.it("Getting bit at 0 from zero returns 0", () => assert.equal(bw.get(0, 0), 0));
+	mocha.it("Getting bit at 1 from zero returns 0", () => assert.equal(bw.get(0, 1), 0));
+	mocha.it("Getting 1 bit at 0 returns 1", () => assert.equal(bw.get(b1101, 0), 1));
+	mocha.it("Getting 0 bit at 1 returns 0", () => assert.equal(bw.get(b1101, 1), 0));
+	mocha.it("Getting bit at large index returns 0", () => assert.equal(bw.get(b1101, 10), 0));
 });
 
 mocha.describe("set()", () => {
-	"set(0, 0) === 1" // Setting bit at 0 in zero returns 1
-	"set(0, 1) === 0b10" // Setting bit at 1 in zero returns 2
-	"set(0b1101, 0) === 0b1101" // Setting 1 bit at 0 remains the number unchanged
-	"set(0b1101, 1) === 0b1111" // Setting 0 bit at 1 sets the bit to 1
-	"set(0b1101, 10) === 0b10000001101" // Setting bit at large index returns large number
+	mocha.it("Setting bit at 0 in zero returns 1", () => assert.equal(bw.set(0, 0), 1));
+	mocha.it("Setting bit at 1 in zero returns 2", () => assert.equal(bw.set(0, 1), 0b10));
+	mocha.it("Setting 1 bit at 0 remains the number unchanged", () => assert.equal(bw.set(b1101, 0), b1101));
+	mocha.it("Setting 0 bit at 1 sets the bit to 1", () => assert.equal(bw.set(b1101, 1), 0b1111));
+	mocha.it("Setting bit at large index returns large number", () => assert.equal(bw.set(b1101, 10), 0b10000001101));
 });
 
 mocha.describe("unset()", () => {
-	"unset(0, 0) === 0" // Unsetting bit at 0 in zero returns 0
-	"unset(0, 1) === 0" // Unsetting bit at 1 in zero returns 0
-	"unset(0b1101, 0) === 0b1100" // Unsetting 1 bit at 0 unsets the bit to 0
-	"unset(0b1101, 1) === 0b1101" // Unsetting 0 bit at 1 remains the number unchanged
-	"unset(0b1101, 10) === 0b1101" // Unsetting bit at large index remains the number unchanged
+	mocha.it("Unsetting bit at 0 in zero returns 0", () => assert.equal(bw.unset(0, 0), 0));
+	mocha.it("Unsetting bit at 1 in zero returns 0", () => assert.equal(bw.unset(0, 1), 0));
+	mocha.it("Unsetting 1 bit at 0 unsets the bit to 0", () => assert.equal(bw.unset(b1101, 0), 0b1100));
+	mocha.it("Unsetting 0 bit at 1 remains the number unchanged", () => assert.equal(bw.unset(b1101, 1), b1101));
+	mocha.it("Unsetting bit at large index remains the number unchanged", () => assert.equal(bw.unset(b1101, 10), b1101));
 });
 
 mocha.describe("toggle()", () => {
-	"toggle(0, 0) === 1" // Toggling bit at 0 in zero returns 1
-	"toggle(0, 1) === 0b10" // Toggling bit at 1 in zero returns 2
-	"toggle(0b1101, 0) === 0b1100" // Toggling 1 bit at 0 toggles the bit to 0
-	"toggle(0b1101, 1) === 0b1111" // Toggling 0 bit at 1 toggles the bit to 1
-	"toggle(0b1101, 10) === 0b10000001101" // Togging bit at large index returns large number
-	"" // Toggling number twice returns the initial number
+	mocha.it("Toggling bit at 0 in zero returns 1", () => assert.equal(bw.toggle(0, 0), 1));
+	mocha.it("Toggling bit at 1 in zero returns 2", () => assert.equal(bw.toggle(0, 1), 0b10));
+	mocha.it("Toggling 1 bit at 0 toggles the bit to 0", () => assert.equal(bw.toggle(b1101, 0), 0b1100));
+	mocha.it("Toggling 0 bit at 1 toggles the bit to 1", () => assert.equal(bw.toggle(b1101, 1), 0b1111));
+	mocha.it("Toggling bit at large index returns large number", () => assert.equal(bw.toggle(b1101, 10), 0b10000001101));
+	mocha.it("Toggling number twice returns the initial number", () => assert.equal(bw.toggle(bw.toggle(b1101, 2), 2), b1101));
 });
 
 mocha.describe("toArray()", () => {
-	"toArray(0) == [0]" // Converting zero to array returns [0]
-	"toArray(0b1101) == [1, 0, 1, 1]" // Converting to array returns an array with reversed order of bits
-	"" // Converting to array and back to number returns the initial number
+	mocha.it("Converting zero to array returns [0]", () => assert.deepStrictEqual(bw.toArray(0), [0]));
+	mocha.it("Converting to array returns an array with reversed order of bits", () => assert.deepStrictEqual(bw.toArray(b1101), [1, 0, 1, 1]));
+	mocha.it("Converting to array and back to number returns the initial number", () => assert.equal(bw.fromArray(bw.toArray(b1101)), b1101));
 });
 
 mocha.describe("fromArray()", () => {
-	"fromArray([0]) == 0" // Converting [0] to number returns 0
-	"fromArray([0, 0]) == 0" // Converting an array filled up with zeroes returns 0
-	"fromArray([1, 0, 1, 1])" // Converting an array returns the number with reversed order of bits
-	"" // Converting from array and back to it returns the initial array
+	mocha.it("Converting [0] to number returns 0", () => assert.equal(bw.fromArray([0]), 0));
+	mocha.it("Converting an array filled up with zeroes returns 0", () => assert.equal(bw.fromArray([0, 0]), 0));
+	mocha.it("Converting an array returns the number with reversed order of bits", () => assert.equal(bw.fromArray([1, 0, 1, 1]), b1101));
+	mocha.it("Converting from array and back to it returns the initial array", () => assert.deepStrictEqual(bw.toArray(bw.fromArray([1, 0, 1, 1])), [1, 0, 1, 1]));
 });
